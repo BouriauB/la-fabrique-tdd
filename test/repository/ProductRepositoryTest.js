311e50dd1bfcd0a7288271a20651273a2a013851
BouriauB <47318623+BouriauB@users.noreply.github.com>
2020-07-03 09:27:20 +0200

    added product test scenarios

--- test/repository/ProductRepositoryTest.js
+++ test/repository/ProductRepositoryTest.js
@@ -28,15 +28,87 @@ describe('ProductRepository', () => {
     });
 
     describe('#create', () => {
+        it('should return new key (Nominal case)', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
 
+        it('should raise error if the key is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should raise error if the value is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should raise error if the value {name} is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should raise error if the value {description} is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should raise error if the value {prize} is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should raise error if the value {categoryId} is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should used default value {images} if she is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should raise error if the value {userId} is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should raise error if the value {categoryId} does not exist', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+
+        it('should raise error if the value {userId} does not exist', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
     });
 
     describe('#update', () => {
-
+        it('should raise error if the id is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
     });
 
     describe('#remove', () => {
-
+        it('should raise error if the id is not defined', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
     });
 
 });
