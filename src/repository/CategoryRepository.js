dc0b575d97c5738ca91b370b5cc7a64f6674d28c
EwenBALOUIN <46872095+EwenBALOUIN@users.noreply.github.com>
2020-07-03 12:38:53 +0200

    :white_check_mark: adding tests for CategoryRepository

--- src/repository/CategoryRepository.js
+++ src/repository/CategoryRepository.js
@@ -1,14 +1,25 @@
 module.exports = class CategoryRepository {
+    constructor() {
+        this.array = [];  //expected_array = [ { 1, "billard" }, { 2, "baby-foot" } ]
+    }
 
-    create(value){
+    create(id, value){
         return null;
     }
 
-    getById(Id){
+    update(id, value){
         return null;
     }
 
-    getAll(){
+    delete(id){
+        return null;
+    }
+
+    getById(id){
+        return null;
+    }
+
+    all(){
         return null;
     }
 
--- test/repository/CategoryRepositoryTest.js
+++ test/repository/CategoryRepositoryTest.js
@@ -2,25 +2,142 @@ const assert = require('assert');
 const CategoryRepository = require('../../src/repository/CategoryRepository');
 
 describe('CategoryRepository', () => {
-
-    describe('#getById', () => {
-
+    beforeEach(() => {
+        categoryRepository = new CategoryRepository();
     });
-
-    describe('#getAll', () => {
-
-    });
-
     describe('#create', () => {
+        it("new object is add", () => {
+            //arrange
+            const expected_name = "pepito";
+            //act
+            categoryRepository.create(1, "pepito");
+            let category = categoryRepository.getById(1);
+            //assert
+            assert.equal(category.name, expected_name);
+        });
 
+        it("new object has not been add no key", () => {
+            //act
+            try {
+                categoryRepository.create(undefined, "pepito");
+            } catch (e){
+                res = true
+            }
+            //assert
+            assert.throws(categoryRepository.create);
+        });
+
+        it("new object has not been add no value", () => {
+            //act
+            try {
+                categoryRepository.create(1, undefined);
+            } catch (e){
+                res = true
+            }
+            //assert
+            assert.throws(categoryRepository.create);
+        });
     });
 
     describe('#update', () => {
+        it("update object", () => {
+            //arrange
+            const expected_name = "topito";
+            categoryRepository.create(1, "pepito");
+            let category_created = categoryRepository.getById(1);
+            //act
+            categoryRepository.update(category_created.id, "topito");
+            let category_updated = categoryRepository.getById(1);
+            //assert
+            assert.equal(category_updated.name, expected_name);
+        });
 
+        it("update object has no value", () => {
+            //act
+            categoryRepository.create(1, "pepito");
+            let category_created = categoryRepository.getById(1);
+            try {
+                categoryRepository.update(category_created.id, undefined);
+            } catch (e){
+                res = true
+            }
+            //assert
+            assert.throws(categoryRepository.update);
+        });
     });
 
-    describe('#remove', () => {
+    describe('#delete', () => {
+        it("delete object", () => {
+            //arrange
+            categoryRepository.create(1, "pepito");
+            //act
+            categoryRepository.delete(1);
+            try {
+                categoryRepository.getById(1);
+            } catch (e){
+                res = true
+            }
+            //assert
+            assert.throws(categoryRepository.getById);
+        });
+        it("delete object but object not exist", () => {
+            //act
+            try {
+                categoryRepository.delete(1);
+            } catch (e){
+                res = true
+            }
+            //assert
+            assert.throws(categoryRepository.delete);
+        });
+        it("delete object but id in param", () => {
+            //act
+            try {
+                categoryRepository.delete(undefined);
+            } catch (e){
+                res = true
+            }
+            //assert
+            assert.throws(categoryRepository.delete);
+        });
+    });
 
+    describe('#getById', () => {
+        it("get object added but no id", () => {
+            //arrange
+            categoryRepository.create(1, "pepito");
+            //act
+            try {
+                categoryRepository.getById();
+            } catch (e){
+                res = true
+            }
+            //assert
+            assert.throws(categoryRepository.create);
+        });
+    });
+
+    describe('#getAll', () => {
+        it("return empty list", () => {
+            //arrange
+            const expected_data = [];
+            let result = null;
+            //act
+            result = categoryRepository.all()
+            //assert
+            assert.equal(JSON.stringify(result), JSON.stringify(expected_data))
+        });
+
+        it("return fill list", () => {
+            //arrange
+            const expected_data = [{1: 'baby-foot'}, {2: 'billard'}];
+            let result = null;
+            //act
+            result = categoryRepository.all()
+            //assert
+            assert.equal(JSON.stringify(result), JSON.stringify(expected_data))
+            assert.equal(JSON.stringify(result).length, JSON.stringify(expected_data).length)
+        });
     });
 
 });
