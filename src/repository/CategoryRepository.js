d686db1b63fc44fe566e999f1ad6be70ac3d95d5
MorioLylian <47317919+MorioLylian@users.noreply.github.com>
2020-07-03 18:13:26 +0200

    :sparkles: implement code in CategoryRepository

--- src/repository/CategoryRepository.js
+++ src/repository/CategoryRepository.js
@@ -1,26 +1,64 @@
 module.exports = class CategoryRepository {
     constructor() {
-        this._array = [];  //expected_array = [ { 1, "billard" }, { 2, "baby-foot" } ]
+        this._array = [];  //expected_array = [ { id: 1, name: "billard" }, { id: 2, name: "baby-foot" } ]
     }
 
-    create(id, value){
-        return null;
+    create(id, name){
+
+        if(id === undefined) {
+            throw new Error();
+        }
+
+        if(name === undefined) {
+            throw new Error();
+        }
+
+        if( this._array[id - 1] !== undefined){
+            throw new Error();
+        }
+
+        let category = {};
+        category['id'] = id;
+        category['name'] = name;
+        this._array.push(category);
     }
 
-    update(id, value){
-        return null;
+    update(id, name){
+        if(id === undefined) {
+            throw new Error();
+        }
+
+        if(name === undefined) {
+            throw new Error();
+        }
+        let category = this.getById(id);
+        if( this._array[id - 1] !== undefined){
+            throw new Error();
+        }
+        category['id'] = id;
+        category['name'] = name;
+        this._array.push(category);
     }
 
     delete(id){
-        return null;
-    }
+        this.getById(id);
+        if(id === undefined) {
+            throw new Error();
+        }
+        this._array.slice(id-1,1)
+    };
 
     getById(id){
-        return null;
-    }
+        if(this._array[id - 1] !== undefined){
+            return this._array[id - 1];
+        }
+        else {
+            throw new Error();
+        }
+    };
 
     getAll(){
-        return null;
+        return this._array;
     }
 
-};
+}
