4db3cb484018deb5a29c2358e230b106fb760664
MorioLylian <47317919+MorioLylian@users.noreply.github.com>
2020-07-03 16:09:30 +0200

    :white_check_mark: add test for userRepository

--- src/model/User.js
+++ src/model/User.js
@@ -1,15 +1,52 @@
 module.exports = class User {
 
-    save(value){
-        return null;
+    constructor(firstname, lastname, email, password, roleId) {
+        this._firstname = firstname;
+        this._lastname = lastname;
+        this._email = email;
+        this._password = password;
+        this._roleId = roleId;
     }
 
-    getByIndex(index){
-        return null;
-    }
+    _getFirstName(){
+      return this._firstname;
+    };
 
-    getAll(){
-        return null;
-    }
+    _setFirstName(firstname){
+        this._firstname = firstname;
+    };
+
+    _getLastName(){
+      return this._lastname;
+    };
+
+    _setLastName(lastname){
+        this._lastname = lastname;
+    };
+
+    _getEmail(){
+      return this._email;
+    };
+
+    _setEmail(email){
+        this._email = email;
+    };
+
+    _getPassword(){
+      return this._password;
+    };
+
+    _setPassword(password){
+        this._password = password;
+    };
+
+    _getRoleId(){
+      return this._roleId;
+    };
+
+    _setRoleId(roleId){
+        this._roleId = roleId;
+    };
+
 
 };
